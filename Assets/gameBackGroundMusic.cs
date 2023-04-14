8805c51a36677b08a136010d129fed31689fd8fe
adobecrack-cc-2022 <105333261+adobecrack-cc-2022@users.noreply.github.com>
2023-04-14 20:59:32 +0800

    Part8

---
+++ Assets/gameBackGroundMusic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameBackGroundMusic : MonoBehaviour
+{
+    [SerializeField] private AudioSource backgroundAudio;
+    [SerializeField] private AudioClip[] audio;
+    [SerializeField] private levelComplete levelFinish;
+    [SerializeField] private GameOverButtonScripts gameOver;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelFinish.isLevelComplete==true&& gameOver.isGameOver==false)
+        {
+            switchMusictoGameplayMusic(audio[0]);
+        }
+        if (levelFinish.isLevelComplete == false && gameOver.isGameOver == true)
+        {
+            switchMusictoGameplayMusic(audio[1]);
+        }
+    }
+    public void switchMusictoGameplayMusic(AudioClip GPM)
+    {
+        backgroundAudio.Stop();
+        backgroundAudio.clip = GPM;
+        backgroundAudio.Play();
+    }
+}
